611912b09942e7931ded8f1093a7c4c2ba4795fc
Jeremie-pires <piresjeremie.mailpro@gmail.com>
2024-11-28 12:10:01 +0100

    maj tip vip

--- tp vip/script.js
+++ tp vip/script.js
@@ -1,69 +1,61 @@
-const saveLocal = () => {
-    localStorage.setItem('personnes', JSON.stringify(personnes));
-}
-
 class Personne {
-    constructor(prenom, nom, status) {
+    constructor(prenom, nom) {
         this.prenom =  prenom;
         this.nom = nom;
-        this.status = false;
+        this.status = true;
     }
 }
 
-const personnes = [];
+let personnes = [];
+
+const saveLocal = () => {
+    localStorage.setItem('personnes', JSON.stringify(personnes));
+}
 
 const afficher = () => {
     const tbody = document.getElementById('myTbody');
     tbody.innerHTML = '';
     for (let personne of personnes) {
-        console.log(personnes);
-
         const template = document.getElementById("templateTr");
         const clone = template.content.cloneNode(true);
-        let td1 = clone.document.getElementById("prenom");
-        td1.textContent = personne.prenom;
-        let td2 = clone.document.getElementById("nom");
-        td2.textContent = personne.nom;
+        let tr = clone.querySelector("tr");
+        let td = clone.querySelectorAll('td');
 
+        td[0].textContent = personne.prenom;
+        td[1].textContent = personne.nom;
 
-        let sup = clone.querySelector("btn btn-danger");
+        if (personne.status) {
+            tr.classList.add('vert');
+            tr.classList.remove('rouge');
+        } else {
+            tr.classList.add('rouge');
+            tr.classList.remove('vert');
+        }
+
+        sup = clone.querySelector(".btn-danger");   
         
         sup.onclick = (event) => {
-            if (confirm("Voulez-vous enlever : " + personne.prenom + ' ' + personne.nom + " ?")) {
-                const indice = event.target.closest("tr").rowIndex - 1;
+                let indice = event.target.closest("tr").rowIndex - 1;
                 personnes.splice(indice, 1);
                 saveLocal();
                 afficher();
-            }
-        };
+            };
 
-        let invit = clone.querySelector("btn btn-warning");
+        invit = clone.querySelector(".btn-warning");
         invit.onclick = (event) => {
-            if (personne.status == false) {
-                personne.status = true;
-                let couleurElement = document.getElementById('box');
-                couleurElement.classList.remove('couleurElement')
-                couleurElement.classList.add('vert');
+            personnes[index].status = !personnes[index].status;
                 saveLocal();
                 afficher();
-            }
-            else if (personne.status == true) {
-                personne.status = false;
-                let couleurElement = document.getElementById('box');
-                couleurElement.classList.remove('couleurElement');
-                couleurElement.classList.add('rouge');
-                saveLocal();
-                afficher();
-            }
+
         };
         tbody.appendChild(clone);
     }
 };
 
 document.getElementById('btnAjouter').onclick = () => {
-    const prenom = document.getElementById('inputprenom').value;
+    let prenom = document.getElementById('inputprenom').value;
     document.getElementById('inputprenom').value = '';
-    const nom = document.getElementById('inputnom').value;
+    let nom = document.getElementById('inputnom').value;
     document.getElementById('inputnom').value = '';
     const personne = new Personne(prenom, nom);
     personnes.push(personne);
@@ -72,7 +64,7 @@ document.getElementById('btnAjouter').onclick = () => {
 };
 
 const data = localStorage.getItem('personnes');
-id (data){
+if (data){
     personnes=JSON.parse(data);
     afficher();
 }
